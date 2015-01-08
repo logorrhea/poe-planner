195013f16a0e8226856790b3821d1b5587cb3c93
Tyler Funk <tfunk@fastmail.fm>
2015-01-08 11:19:34 -0600

    Chopped up skill sprite sheets.
    Added a Skill Frame active/inactive class

---
+++ Assets/skill_nodes/SkillFrame.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SkillFrame : MonoBehaviour {
+
+	public Sprite unallocatedSprite;
+	public Sprite allocatedSprite;
+
+	public bool allocated;
+
+	private SpriteRenderer renderer;
+
+	// Use this for initialization
+	void Start () {
+		renderer = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (allocated) {
+			renderer.sprite = allocatedSprite;
+		} else {
+			renderer.sprite = unallocatedSprite;
+		}
+	}
+}
