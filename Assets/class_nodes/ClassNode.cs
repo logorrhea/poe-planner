a698695589728cd0cd124e69a27b8ccc2117ff7b
Tyler Funk <tfunk@fastmail.fm>
2015-01-08 08:58:44 -0600

    Changed start nodes to swap sprites if active

---
+++ Assets/class_nodes/ClassNode.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class ClassNode : MonoBehaviour {
+
+	public Sprite activeSprite;
+	public Sprite inactiveSprite;
+
+	public bool active = false;
+
+	private SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (active) {
+			spriteRenderer.sprite = activeSprite;
+		} else {
+			spriteRenderer.sprite = inactiveSprite;
+		}
+	}
+}
